d74dadd7734c860edf33dd7b391f65c5b3f3e948
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 15:11:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateIndex() {
+  const numApplicants = parseFloat(document.getElementById("num-aplicants").value);
+  const numDesistences = parseFloat(document.getElementById("num-desistences").value);
+  const index = (numDesistences / numApplicants) * 100;
+  document.getElementById("result").innerHTML = `Índice de desistências do processo seletivo: ${index.toFixed(2)}%`;
+}
 
